d4ef7425d652b35236e61446d0615f5f7254b88e
Omer <adnan.uzulmez@perwatch.com>
2023-04-28 16:42:52 +0300

    Date filter UI added

--- src/App.vue
+++ src/App.vue
@@ -37,6 +37,18 @@
               d="M5 11.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm-3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm0 4a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm0 4a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
           </svg>
         </div>
+        <div class="sidebarSwitch" @click.self="calendarShow()">
+          <div class="hideContent" :class="[dateFlag === 0 ? 'hideContent-show' : '']">
+            <date-picker v-model="dateRange" range></date-picker>
+          </div>
+          <svg @click="calendarShow()" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+            class="bi bi-calendar-date" viewBox="0 0 16 16" style=" width: 20px;height: 20px;">
+            <path
+              d="M6.445 11.688V6.354h-.633A12.6 12.6 0 0 0 4.5 7.16v.695c.375-.257.969-.62 1.258-.777h.012v4.61h.675zm1.188-1.305c.047.64.594 1.406 1.703 1.406 1.258 0 2-1.066 2-2.871 0-1.934-.781-2.668-1.953-2.668-.926 0-1.797.672-1.797 1.809 0 1.16.824 1.77 1.676 1.77.746 0 1.23-.376 1.383-.79h.027c-.004 1.316-.461 2.164-1.305 2.164-.664 0-1.008-.45-1.05-.82h-.684zm2.953-2.317c0 .696-.559 1.18-1.184 1.18-.601 0-1.144-.383-1.144-1.2 0-.823.582-1.21 1.168-1.21.633 0 1.16.398 1.16 1.23z" />
+            <path
+              d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z" />
+          </svg>
+        </div>
         <div class="sidebarSwitch" @click.self="filterShow()">
           <div class="hideContent" :class="[filterFlag === 0 ? 'hideContent-show' : '']">
             <div class="magnitudeFilter">
@@ -50,18 +62,7 @@
               d="M6 10.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z" />
           </svg>
         </div>
-        <div class="sidebarSwitch" @click.self="calendarShow()">
-          <div class="hideContent" :class="[dateFlag === 0 ? 'hideContent-show' : '']">
-            <date-picker v-model="dateRange" range></date-picker>
-          </div>
-          <svg @click="calendarShow()" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-            class="bi bi-calendar-date" viewBox="0 0 16 16">
-            <path
-              d="M6.445 11.688V6.354h-.633A12.6 12.6 0 0 0 4.5 7.16v.695c.375-.257.969-.62 1.258-.777h.012v4.61h.675zm1.188-1.305c.047.64.594 1.406 1.703 1.406 1.258 0 2-1.066 2-2.871 0-1.934-.781-2.668-1.953-2.668-.926 0-1.797.672-1.797 1.809 0 1.16.824 1.77 1.676 1.77.746 0 1.23-.376 1.383-.79h.027c-.004 1.316-.461 2.164-1.305 2.164-.664 0-1.008-.45-1.05-.82h-.684zm2.953-2.317c0 .696-.559 1.18-1.184 1.18-.601 0-1.144-.383-1.144-1.2 0-.823.582-1.21 1.168-1.21.633 0 1.16.398 1.16 1.23z" />
-            <path
-              d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z" />
-          </svg>
-        </div>
+
       </div>
     </div>
     <div id="mapBox">
@@ -430,6 +431,7 @@ body {
   box-shadow: rgb(173 173 173 / 55%) 0px 0px 7px 6px;
   cursor: pointer;
   height: 28px;
+  width: 28px;
 }
 
 .sidebarSwitch svg {
@@ -609,6 +611,13 @@ body {
   -webkit-overflow-scrolling: touch;
 }
 
+.mx-datepicker-popup{
+  left: 644px !important;
+}
+
+.mx-datepicker-range {
+    width: 220px !important;
+}
 /*https://api.orhanaydogdu.com.tr/deprem/kandilli/archive?limit=1000&date_end=2023-04-28&date=2023-04-01&skip=5000*/
 
 @keyframes pulse {
@@ -626,4 +635,5 @@ body {
     transform: scale(0.95);
     box-shadow: 0 0 0 0 rgba(255, 255, 255, 0);
   }
-}</style>
+}
+</style>
