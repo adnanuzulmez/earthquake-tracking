8b612b3789a160e1a3bccdec716a20839a3b2901
Omer <adnan.uzulmez@perwatch.com>
2023-04-28 15:38:26 +0300

    Magnitude and Date Filter Added

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,74 @@
 <template>
   <div id="app">
-   
-   <div class="leftSidebar" :class="[sidebarFlag === 0 ? 'sidebarHidden' : '']">
-    <div id="sideBar" >
-      <div class="searchBar">
-        <input type="text" placeholder="Search" v-model="citySearch">
-      </div>
-      <div class="locationData" v-for="(item, index) in searchArray[0]" :key="index"
-        :class="[(index === 0 && citySearch === '') ? 'lastLocation' : '']" @click="setCenterByLocation(item._id, index)">
-        <div class="headInfo">
-          <h3 class="locationNumber magSize" style=" cursor: pointer;">
-            {{ item.mag }}</h3>
-          <h4>●</h4>
-          <h3 class="locationNumber fw-300" style=" cursor: pointer;">
-            {{ item.depth }} KM</h3>
-          <h4>●</h4>
-          <h3 class="locationNumber fw-300" style=" cursor: pointer;">
-            {{ dateDay[index].date }} {{ dateDay[index].time }}</h3>
+
+    <div class="leftSidebar" :class="[sidebarFlag === 0 ? 'sidebarHidden' : '']">
+      <div id="sideBar">
+        <div class="searchBar">
+          <input type="text" placeholder="Search" v-model="citySearch">
         </div>
-        <div>
-          <div class="lastLocationData">
-            <p class="locationAddress" style=" cursor: pointer;">
-              {{ item.title }}
-            </p>
-            <p class="lastTitle" v-if="index === 0 && citySearch === ''">Last</p>
+        <div class="locationData" v-for="(item, index) in searchArray[0]" :key="index"
+          :class="[(index === 0 && citySearch === '' && magValue === '') ? 'lastLocation' : '']"
+          @click="setCenterByLocation(item._id, index)">
+          <div class="headInfo">
+            <h3 class="locationNumber magSize" style=" cursor: pointer;">
+              {{ item.mag }}</h3>
+            <h4>●</h4>
+            <h3 class="locationNumber fw-300" style=" cursor: pointer;">
+              {{ item.depth }} KM</h3>
+            <h4>●</h4>
+            <h3 class="locationNumber fw-300" style=" cursor: pointer;">
+              {{ dateDay[index].date }} {{ dateDay[index].time }}</h3>
+          </div>
+          <div>
+            <div class="lastLocationData">
+              <p class="locationAddress" style=" cursor: pointer;">
+                {{ item.title }}
+              </p>
+              <p class="lastTitle" v-if="index === 0 && citySearch === '' && magValue === ''">Last</p>
+            </div>
           </div>
         </div>
       </div>
+      <div class="filterButtons">
+        <div class="sidebarSwitch" @click="sidebarHide()" :class="[sidebarFlag === 0 ? 'sidebarSwitchStatus' : '']">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list-ul"
+            viewBox="0 0 16 16">
+            <path fill-rule="evenodd"
+              d="M5 11.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm-3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm0 4a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm0 4a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
+          </svg>
+        </div>
+        <div class="sidebarSwitch" @click.self="filterShow()">
+          <div class="hideContent" :class="[filterFlag === 0 ? 'hideContent-show' : '']">
+            <div class="magnitudeFilter">
+              <multiselect v-model="magValue" :options="magOptions" :searchable="false" :close-on-select="true"
+                :show-labels="false" placeholder="Select Magnitude"></multiselect>
+            </div>
+          </div>
+          <svg @click="filterShow()" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+            class="bi bi-filter" viewBox="0 0 16 16">
+            <path
+              d="M6 10.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z" />
+          </svg>
+        </div>
+        <div class="sidebarSwitch" @click.self="calendarShow()">
+          <div class="hideContent" :class="[dateFlag === 0 ? 'hideContent-show' : '']">
+            <date-picker v-model="dateRange" range></date-picker>
+          </div>
+          <svg @click="calendarShow()" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+            class="bi bi-calendar-date" viewBox="0 0 16 16">
+            <path
+              d="M6.445 11.688V6.354h-.633A12.6 12.6 0 0 0 4.5 7.16v.695c.375-.257.969-.62 1.258-.777h.012v4.61h.675zm1.188-1.305c.047.64.594 1.406 1.703 1.406 1.258 0 2-1.066 2-2.871 0-1.934-.781-2.668-1.953-2.668-.926 0-1.797.672-1.797 1.809 0 1.16.824 1.77 1.676 1.77.746 0 1.23-.376 1.383-.79h.027c-.004 1.316-.461 2.164-1.305 2.164-.664 0-1.008-.45-1.05-.82h-.684zm2.953-2.317c0 .696-.559 1.18-1.184 1.18-.601 0-1.144-.383-1.144-1.2 0-.823.582-1.21 1.168-1.21.633 0 1.16.398 1.16 1.23z" />
+            <path
+              d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z" />
+          </svg>
+        </div>
+      </div>
     </div>
-    <div class="sidebarSwitch" @click="sidebarHide()" :class="[sidebarFlag === 0 ? 'sidebarSwitchStatus' : '']">
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list-ul"
-        viewBox="0 0 16 16">
-        <path fill-rule="evenodd"
-          d="M5 11.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm-3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm0 4a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm0 4a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
-      </svg>
-    </div>
-   </div>
     <div id="mapBox">
       <l-map :zoom="zoom" :center="center" :min-zoom="minZoom" :max-zoom="maxZoom" ref="map">
         <l-tile-layer :url="this.mapCreator" :attribution="attribution" />
         <Vue2LeafletHeatmap :lat-lng="getHeatMapDatas" :radius="40" :min-opacity="0.35" :max-zoom="10" :blur="50">
         </Vue2LeafletHeatmap>
-
         <l-marker v-for="(item, index) in position" :key="index" :lat-lng="item.position"
           @click="() => center = [item.position.lat, item.position.lng]">
           <l-popup>
@@ -59,7 +86,6 @@
             <div :id="item.id" :class="[`mag-${parseInt(item.popup.mag)}`, index === 0 ? 'lastEarthQuake' : '']"></div>
           </l-icon>
           <l-tooltip :content="item.tooltip">
-
           </l-tooltip>
         </l-marker>
         <l-icon-default :image-path="path" />
@@ -70,9 +96,13 @@
 
 <script>
 import { LMap, LTileLayer, LMarker, LIconDefault, LPopup, LIcon, LTooltip } from "vue2-leaflet";
-
 import mapMixin from "./assets/js/map.js"
 import Vue2LeafletHeatmap from "./Vue2LeafletHeatMap";
+import Multiselect from 'vue-multiselect'
+import moment from "moment"
+import DatePicker from 'vue2-datepicker';
+import 'vue2-datepicker/index.css';
+
 let cityId
 export default {
   name: "EarthquakeMap",
@@ -86,6 +116,8 @@ export default {
     Vue2LeafletHeatmap,
     LIcon,
     LTooltip,
+    Multiselect,
+    DatePicker
   },
   data() {
     return {
@@ -99,52 +131,129 @@ export default {
       citySearch: '',
       searchArray: [],
       sidebarFlag: 1,
+      magOptions: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+      magValue: "",
+      filterFlag: 1,
+      dateFlag: 1,
+      dateRange: ""
 
     };
   },
   mounted() {
-
     this.getMap()
     this.getEAdatas = this.getEarthquakeDatas()
-
     console.log(this.getDetailData);
     setTimeout(() => {
       this.createHeatMapDatas()
       this.createMarkerDatas()
-
     }, 400);
     setTimeout(() => {
       this.$refs.map.mapObject.setZoom(5)
     }, 200);
     let leafletZoom = document.querySelector(".leaflet-left")
     leafletZoom.remove()
-
   },
-
-
   watch: {
     citySearch() {
-
       this.filterCity()
-
+    },
+    magValue() {
+      this.filterMag()
     }
   },
   methods: {
-    sidebarHide(){
-      if(this.sidebarFlag != 0){
+    sidebarHide() {
+      if (this.sidebarFlag != 0) {
         this.sidebarFlag = 0
       }
-      else{
+      else {
         this.sidebarFlag = 1
       }
+    },
+    filterMag() {
+      this.searchArray[0] = this.getDetailData[0]
+      this.searchArray[0] = this.searchArray[0].filter(item => parseInt(item.mag) >= parseInt(this.magValue))
+      let tempPosition = []
+      this.position = []
+      this.getHeatMapDatas = []
+      this.dateDay = []
+      tempPosition = this.searchArray[0].filter(item => parseInt(item.mag) >= parseInt(this.magValue))
+      tempPosition.map((item) => {
+
+        this.position.push(
+          {
+            position: {
+              lat: item.geojson.coordinates[1],
+              lng: item.geojson.coordinates[0]
+            },
+            popup: {
+              mag: item.mag,
+              depth: item.depth,
+            },
+            tooltip: item.title,
+            id: item._id
+          })
+        this.getHeatMapDatas.push({ lat: item.geojson.coordinates[1], lng: item.geojson.coordinates[0] })
+        this.dateDay.push(
+          {
+            date: moment(item.date_time).format('dddd'),
+            time: moment(item.date_time).format('HH:mm'),
+            title: item.title
+          })
+      })
     },
     filterCity() {
       this.searchArray[0] = this.getDetailData[0]
       this.searchArray[0] = this.searchArray[0].filter(item => item.title.toLowerCase().includes(this.citySearch.toLowerCase()))
+      let tempLoc = []
+      this.position = []
+      this.getHeatMapDatas = []
+      this.dateDay = []
+      tempLoc = this.searchArray[0].filter(item => item.title.toLowerCase().includes(this.citySearch.toLowerCase()))
+      tempLoc.map((item) => {
+
+        this.position.push(
+          {
+            position: {
+              lat: item.geojson.coordinates[1],
+              lng: item.geojson.coordinates[0]
+            },
+            popup: {
+              mag: item.mag,
+              depth: item.depth,
+            },
+            tooltip: item.title,
+            id: item._id
+          })
+        this.getHeatMapDatas.push({ lat: item.geojson.coordinates[1], lng: item.geojson.coordinates[0] })
+        this.dateDay.push(
+          {
+            date: moment(item.date_time).format('dddd'),
+            time: moment(item.date_time).format('HH:mm'),
+            title: item.title
+          })
+
+      })
+    },
+    filterShow() {
+      if (this.filterFlag != 0) {
+        this.filterFlag = 0
+      }
+      else {
+        this.filterFlag = 1
+      }
+    },
+    calendarShow() {
+      if (this.dateFlag != 0) {
+        this.dateFlag = 0
+      }
+      else {
+        this.dateFlag = 1
+      }
     },
     setCenterByLocation(id) {
-      
-      cityId = this.getDetailData[0].filter((item) => item._id === id) 
+
+      cityId = this.getDetailData[0].filter((item) => item._id === id)
       console.log(cityId[0]);
       setTimeout(async () => {
         this.getHeatMapDatas = []
@@ -162,13 +271,10 @@ export default {
         })
       }, 3300);
     },
-
   }
 };
-
-
 </script>
-
+<style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Montserrat&display=swap');
 @import "~leaflet.markercluster/dist/MarkerCluster.css";
@@ -206,7 +312,7 @@ body {
   position: relative;
   right: 0%;
   z-index: 99999999;
-  transition:  1.5s;
+  transition: 1.5s;
 }
 
 * {
@@ -292,43 +398,50 @@ body {
   margin: 32px 30px 23px 25px;
 }
 
-.sidebarSwitchStatus{
+.sidebarSwitchStatus {
   margin-left: 20px;
 }
 
-.sidebarHidden{
+.sidebarHidden {
   right: 500px !important;
-  transition:  3s;
+  transition: 3s;
 }
 
-.leftSidebar{
-    transition: 1s;
-    display: flex;
-    position: relative;
-    right: 0 
+.leftSidebar {
+  transition: 1s;
+  display: flex;
+  position: relative;
+  right: 0
 }
 
-.sidebarSwitch{
-    
-    position: relative;
-    color: #dfdfdf;
-    margin-left: 20px;
-    z-index: 9999999;
-    padding: 10px;
-    border-radius: 50%;
-    background: #494c50;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    top: 20px;
-    box-shadow: rgb(173 173 173 / 55%) 0px 0px 7px 6px;
-    cursor: pointer;
-    height: 28px;
+.sidebarSwitch {
+
+  position: relative;
+  color: #dfdfdf;
+  margin-left: 20px;
+  margin-top: 10px;
+  z-index: 9999999;
+  padding: 10px;
+  border-radius: 50%;
+  background: #494c50;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  box-shadow: rgb(173 173 173 / 55%) 0px 0px 7px 6px;
+  cursor: pointer;
+  height: 28px;
 }
 
-.sidebarSwitch svg{
-    width: 28px;
-    height: 28px;
+.sidebarSwitch svg {
+  width: 28px;
+  height: 28px;
+}
+
+.filterButtons {
+  display: flex;
+  max-width: 140px;
+  height: 130px;
+  flex-wrap: wrap;
 }
 
 .searchBar input {
@@ -378,13 +491,11 @@ body {
 }
 
 .headInfo {
-
   font-size: 13px;
   display: flex;
   justify-content: space-between;
   margin-bottom: 10px;
   align-items: center;
-
 }
 
 .locationAddress {
@@ -410,24 +521,109 @@ body {
   margin: -6px auto 0px !important;
 }
 
-.lastEarthQuake{
+.multiselect__content-wrapper {
+  max-height: 150px !important;
+}
+
+.multiselect {
+  width: 175px;
+}
+
+.lastEarthQuake {
   animation: pulse 2s infinite;
 }
-	
+
+.hideContent-show {
+  left: 55px !important;
+  opacity: 1 !important;
+  transition: .3s;
+}
+
+.hideContent {
+  position: absolute;
+  left: 100px;
+  opacity: 0;
+  transition: .3s;
+  z-index: -1;
+}
+
+.multiselect__option--selected.multiselect__option--highlight {
+  background: #ff6a6a00;
+  color: #fff;
+}
+
+.multiselect__option--highlight {
+  background: #ffffff1f;
+  color: #fff;
+}
+
+.multiselect__option--selected {
+  background: #ff6a6a00;
+  color: #fff;
+}
+
+.multiselect__option {
+  color: #fff;
+}
+
+.multiselect__tags {
+  min-height: 40px;
+  display: block;
+  padding: 8px 40px 0 8px;
+  border-radius: 5px;
+  border: 1px solid #525252;
+  background: #525252;
+  font-size: 14px;
+  box-shadow: rgb(173 173 173 / 28%) 0px 0px 7px 6px;
+}
+
+.multiselect__single {
+  position: relative;
+  display: inline-block;
+  min-height: 20px;
+  line-height: 20px;
+  border: none;
+  border-radius: 5px;
+  background: #525252;
+  padding: 0 0 0 5px;
+  width: 100%;
+  transition: border .1s ease;
+  box-sizing: border-box;
+  margin-bottom: 8px;
+  vertical-align: top;
+  color: #fff;
+}
+
+.multiselect__content-wrapper {
+  position: absolute;
+  display: block;
+  background: #525252;
+  width: 100%;
+  max-height: 240px;
+  overflow: auto;
+  border: none;
+  border-top: none;
+  border-bottom-left-radius: 5px;
+  border-bottom-right-radius: 5px;
+  z-index: 50;
+  -webkit-overflow-scrolling: touch;
+}
+
+/*https://api.orhanaydogdu.com.tr/deprem/kandilli/archive?limit=1000&date_end=2023-04-28&date=2023-04-01&skip=5000*/
+
 @keyframes pulse {
-	0% {
+  0% {
     transform: scale(0.95);
     box-shadow: 0 0 0 3px rgb(255, 255, 255);
   }
-  
+
   70% {
     transform: scale(1.2);
     box-shadow: 0 0 0 15px rgba(255, 255, 255, 0);
   }
-  
+
   100% {
     transform: scale(0.95);
     box-shadow: 0 0 0 0 rgba(255, 255, 255, 0);
   }
-}
-</style>
+}</style>
